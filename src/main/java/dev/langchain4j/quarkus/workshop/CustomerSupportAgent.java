ed87e801b18b9d050c6e753f87486a2b5864580c
LangChain4j <info@langchain4j.dev>
2024-05-02 10:37:51 +0200

    Step 1: basic customer support agent

--- src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgent.java
+++ src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgent.java
@@ -1,6 +1,5 @@
 package dev.langchain4j.quarkus.workshop;
 
-import dev.langchain4j.service.SystemMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.SessionScoped;
 
@@ -8,6 +7,5 @@ import jakarta.enterprise.context.SessionScoped;
 @RegisterAiService
 public interface CustomerSupportAgent {
 
-    @SystemMessage("You are a customer support agent of a car rental company named 'Miles of Smiles'.")
     String chat(String userMessage);
 }
